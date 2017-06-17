5915eb74eb48a6b23a94a9a7b568cb539f6ed57b
Alan <selfish570@gmail.com>
2017-06-17 21:00:02 +0200

    All files needed.

---
+++ HelloWorld/src/main/java/com/helloworldspringboot/HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.helloworldspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+		System.out.print("Hello World!");
+	}
+}
